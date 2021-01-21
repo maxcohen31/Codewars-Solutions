c6ec708dc7761d2516e50ef888eea8431c70e4e1
Emanuele <73888184+maxcohen31@users.noreply.github.com>
2021-01-21 09:13:53 +0000

    Polygon_area_calculator
    Assignment
    
    In this project you will use object oriented programming to create a Rectangle class and a Square class. The Square class should be a subclass of Rectangle and inherit methods and attributes.
    Rectangle class
    
    When a Rectangle object is created, it should be initialized with width and height attributes. The class should also contain the following methods:
    
        set_width
        set_height
        get_area: Returns area (width * height)
        get_perimeter: Returns perimeter (2 * width + 2 * height)
        get_diagonal: Returns diagonal ((width ** 2 + height ** 2) ** .5)
        get_picture: Returns a string that represents the shape using lines of "*". The number of lines should be equal to the height and the number of "*" in each line should be equal to the width. There should be a new line (\n) at the end of each line. If the width or height is larger than 50, this should return the string: "Too big for picture.".
        get_amount_inside: Takes another shape (square or rectangle) as an argument. Returns the number of times the passed in shape could fit inside the shape (with no rotations). For instance, a rectangle with a width of 4 and a height of 8 could fit in two squares with sides of 4.
    
    Additionally, if an instance of a Rectangle is represented as a string, it should look like: Rectangle(width=5, height=10)
    Square class
    
    The Square class should be a subclass of Rectangle. When a Square object is created, a single side length is passed in. The __init__ method should store the side length in both the width and height attributes from the Rectangle class.
    
    The Square class should be able to access the Rectangle class methods but should also contain a set_side method. If an instance of a Square is represented as a string, it should look like: Square(side=9)
    
    Additionally, the set_width and set_height methods on the Square class should set both the width and height.

---
+++ Freecodecamp/Polygon_area_calculator.py
@@ -0,0 +1,70 @@
+# Polygon Area Calculator
+
+from math import sqrt
+
+class Rectangle():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, chosen_width):
+        self.width = chosen_width
+
+    def set_height(self, chosen_height):
+        self.height = chosen_height
+
+    def __str__(self):
+         return 'Rectangle(width = {}, height = {})'.format(self.width, self.height)
+
+    def area(self):
+        self.area = self.width * self.height
+        return self.area
+
+    def get_diagonal(self):
+        self.diagonal = sqrt(self.width ** 2 + self.height ** 2)
+        return self.diagonal
+
+    def perimeter(self):
+        self.perimeter = self.width * 2 + self.height * 2
+        return self.perimeter
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+             print('Too big for picture')
+
+        else:
+            for w in range(self.width):
+                for h in range(self.height):
+                    print('*', end = '')
+                print()
+
+
+    def get_amount_inside(self, new_shape):
+        return self.area() // new_shape.area()
+
+
+class Square(Rectangle):
+
+    def __init__(self, side):
+        super().__init__(side, side)
+        self.side = side
+
+    def set_side(self, side):
+            self.width = side
+            self.height = side
+            return self.width, self.height
+
+    def __str__(self):
+        return 'Square(side = {})'.format(self.side)
+
+
+
+
+
+
+
+
+
+
+
+
