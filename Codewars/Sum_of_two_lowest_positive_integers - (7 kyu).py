6be8bcebf4192b152d7c98c490ffb6288eac0f3a
Emanuele <eman_93@hotmail.it>
2021-06-29 16:04:36 +0000

    Codewars Kata

---
+++ Codewars/Sum_of_two_lowest_positive_integers - (7 kyu).py
@@ -0,0 +1,13 @@
+def sum_two_smallest_numbers(numbers):
+    x = sorted(numbers)
+    for i in range(0, 1):
+        summ = 0
+        summ = x[0] + x[1]
+    return summ
+
+
+
+l = [14, 13 ,84 ,72, 3]
+
+
+    
