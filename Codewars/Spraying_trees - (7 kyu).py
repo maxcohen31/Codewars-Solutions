7f81e3b854391dc9c5052875536a4281a50f0cae
Emanuele <eman_93@hotmail.it>
2021-08-17 13:38:42 +0000

    Codewars Kata

---
+++ Codewars/Spraying_trees - (7 kyu).py
@@ -0,0 +1,3 @@
+def task(w,n,c):
+    v = {'Monday':'James','Tuesday':'John','Wednesday':'Robert','Thursday':'Michael','Friday':'William'}
+    return f'It is {w} today, {v[w]}, you have to work, you must spray {n} trees and you need {n*c} dollars to buy liquid'
