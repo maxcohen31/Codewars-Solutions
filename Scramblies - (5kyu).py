d453039faf22868b2f497473b013e59acc268351
Emanuele <eman_93@hotmail.it>
2021-10-22 16:35:02 +0000

    Delete Scramblies - (5kyu).py

--- Scramblies - (5kyu).py
+++
@@ -1,8 +0,0 @@
-def scramble(x, y):
-    if len(x) == 0 or len(y) == 0:
-        return False
-    for letters in set(y):
-        if x.count(letters) < y.count(letters):
-            return False
-    return True
-
