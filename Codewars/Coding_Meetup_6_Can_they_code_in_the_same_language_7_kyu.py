dfd7ba9b5da68a9c8bb0fcf0e06b671cdf0981bb
Emanuele <eman_93@hotmail.it>
2022-03-04 17:02:43 +0000

    Codewars Kata

---
+++ Codewars/Coding_Meetup_6_Can_they_code_in_the_same_language_7_kyu.py
@@ -0,0 +1,12 @@
+def is_same_language(lst): 
+    return len(set([person['language'] for person in lst])) == 1
+
+
+
+list1 = [
+  { 'firstName': 'Daniel', 'lastName': 'J.', 'country': 'Aruba', 'continent': 'Americas', 'age': 42, 'language': 'JavaScript' },
+  { 'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus', 'continent': 'Europe', 'age': 22, 'language': 'JavaScript' },
+  { 'firstName': 'Hanna', 'lastName': 'L.', 'country': 'Hungary', 'continent': 'Europe', 'age': 65, 'language': 'JavaScript' },
+]
+
+print(is_same_language(list1))
