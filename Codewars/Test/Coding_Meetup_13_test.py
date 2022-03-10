77fa569b34536833d41c6423f720aa4f9f547a41
Emanuele <eman_93@hotmail.it>
2022-03-10 18:04:37 +0000

    test

---
+++ Codewars/Test/Coding_Meetup_13_test.py
@@ -0,0 +1,29 @@
+from Coding_Meetup_13_Is_the_meetup_language_diverse_6_kyu import is_language_diverse
+import unittest
+
+class DiverseLanguage(unittest.TestCase):
+    def test_1(self):
+        data = [
+            { 'firstName': 'Daniel', 'lastName': 'J.', 'country': 'Aruba', 'continent': 'Americas', 'age': 42, 'language': 'Python' },
+            { 'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus', 'continent': 'Europe', 'age': 22, 'language': 'Ruby' },
+            { 'firstName': 'Sou', 'lastName': 'B.', 'country': 'Japan', 'continent': 'Asia', 'age': 43, 'language': 'Ruby' },
+            { 'firstName': 'Hanna', 'lastName': 'L.', 'country': 'Hungary', 'continent': 'Europe', 'age': 95, 'language': 'JavaScript' },
+            { 'firstName': 'Jayden', 'lastName': 'P.', 'country': 'Jamaica', 'continent': 'Americas', 'age': 18, 'language': 'JavaScript' },
+            { 'firstName': 'Joao', 'lastName': 'D.', 'country': 'Portugal', 'continent': 'Europe', 'age': 25, 'language': 'JavaScript' }
+            ]
+        result = False
+        self.assertEquals(is_language_diverse(data), result)
+        
+        
+    def test_2(self):
+        data = [
+            { 'firstName': 'Daniel', 'lastName': 'J.', 'country': 'Aruba', 'continent': 'Americas', 'age': 42, 'language': 'Python' },
+            { 'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus', 'continent': 'Europe', 'age': 22, 'language': 'Ruby' },
+            { 'firstName': 'Sou', 'lastName': 'B.', 'country': 'Japan', 'continent': 'Asia', 'age': 43, 'language': 'Ruby' }
+            ]     
+        result = True
+        self.assertEquals(is_language_diverse(data), result)
+        
+        
+if __name__ == "__main__":
+    unittest.main()        
