cda49f1a29d5fc671572b35ce8753e8cb9bcc0e1
Emanuele <eman_93@hotmail.it>
2022-03-23 18:39:22 +0000

    Codewars Kata

---
+++ Codewars/Coding_Meetup_2_Greet_Developers_7_kyu.py
@@ -0,0 +1,12 @@
+def greet_developers(lst): 
+    for person in lst:
+        person['greeting'] = f"Hi {person['firstName']}, what do you like the most about {person['language']}?"
+    return lst
+
+list1 = [
+  { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
+  { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
+  { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' } 
+]
+
+print(greet_developers(list1))
