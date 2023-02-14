dbd2a291373eaa8db35c5086c4e276024fd6159e
maxcohen <eman_93@hotmail.it>
2023-02-14 14:47:16 +0100

    Codewars Kata

---
+++ Codewars/Coding_Meetup_3_Is_Ruby_coming_7_kyu.py
@@ -0,0 +1,15 @@
+from typing import List
+def is_ruby_coming(lst: List[dict]) -> bool:
+    for dev in lst:
+        if dev["language"] == "Ruby":
+            return True
+    return False
+
+
+list1 = [
+    { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
+    { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
+    { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' } 
+    ]
+
+print(is_ruby_coming(list1))
