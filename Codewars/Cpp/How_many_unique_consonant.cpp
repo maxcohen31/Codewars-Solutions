3c497372923d8671edea2e5a760ff50dc517f3bb
maxcohen31 <eman_93@hotmail.it>
2023-04-04 09:10:50 +0200

    cpp kata

--- Codewars/Cpp/How_many_unique_consonant.cpp
+++ Codewars/Cpp/How_many_unique_consonant.cpp
@@ -7,6 +7,7 @@ unsigned int countConsonants(const std::string& str){
     
     unordered_map<char, int> umap;
 
+ 
     for (int i = 0; i < str.length(); i++) {
         char c = tolower(str[i]);
         if (c != 'a' && c != 'e' && c != 'o' && c != 'u' && c != 'i' &&
