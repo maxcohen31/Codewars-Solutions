810b27b0e83a7717a435d09e0ced20fe5271cfc6
maxcohen31 <eman_93@hotmail.it>
2023-04-24 12:09:45 +0200

    Codewars Kata

---
+++ Codewars/Quadrants_8_kyu.py
@@ -0,0 +1,9 @@
+def quadrant(x: int, y: int) -> int:
+    if x > 0 and y > 0:
+        return 1
+    elif x < 0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    else:
+        return 4
