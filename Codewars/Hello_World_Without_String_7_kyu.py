91cb068e53af9527dd60a20e8fdf6e06601dffea
maxcohen31 <eman_93@hotmail.it>
2023-06-09 11:51:54 +0200

    Codewars Kata

---
+++ Codewars/Hello_World_Without_String_7_kyu.py
@@ -0,0 +1,7 @@
+def hello_world():
+    # Remember, no strings!
+    return chr(72) + chr(101) + chr(108) + chr(108) + chr(111) + chr(44) + chr(32) + chr(87) + chr(111) + chr(114) + chr(108) + chr(100) + chr(33)
+
+hello_world=lambda:bytes([72,101,108,108,111,44,32,87,111,114,108,100,33]).decode()
+
+
